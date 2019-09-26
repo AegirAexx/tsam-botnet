ad28640ef0da82ba45180c0a9b61965a2c50fe62
Ægir Æxx <aegir.tomasson@gmail.com>
2019-09-26 14:41:05 +0000

    refactor the filters

--- validation/scratch_pad.cpp
+++ validation/scratch_pad.cpp
@@ -10,88 +10,76 @@ std::vector<std::byte> getRawBytes(std::string str);
 
 int main(int argc, char *argv[]){
 
-    // PART: STRING => RAW BYTE
+    // // SECTION: STRING => RAW BYTE => STRING
     std::string input("<my command>");
 
-    std::cout << "Plain string: " << input << std::endl;
+    std::cout << "###\nPlain string: " << input << std::endl;
+    std::cout << "Plain string length: " << input.size() << std::endl;
 
     // std::vector<std::byte> rawVec{getRawBytes(input)};
+    std::cout << "###\nByte version of string with SOH/EOT: ";
     auto rawVec{getRawBytes(input)};
-
-    std::cout << "Byte version with SOH/EOT: ";
     for(auto i: rawVec) std::cout << std::hex << (size_t)i << " ";
     std::cout << std::endl;
-    std::cout << "Payload size: " << std::dec << rawVec.size() << " bytes." << std::endl;
+    std::cout << "Length byte vector: " << std::dec << rawVec.size() << " bytes" << std::endl;
 
-    // PART: REGEX KNOWN COMMANDS
+    std::string rebuiltString;
+    for(auto byte: rawVec) rebuiltString.push_back((char)byte);
+    std::cout << "###\nString rebuilt from byte vector: " << rebuiltString << std::endl;
+    std::cout << "String length: " << rebuiltString.size() << std::endl;
 
+
+    // SECTION: REGEX KNOWN COMMANDS
+
+    // COM: List of potential commands.
     std::array<std::string,10> commands{"LEAVE","listservers","keepaLIVE","GET MSG","Servers","send msg","DELETE","statusreq","statusresp","statusrespt"};
 
 
 
+    // PART: ToLower TRANSFORM
     std::cout << "\n######### Initial: " << std::endl;
     for(auto i: commands) std::cout << i << std::endl;
 
-    // ToLower()
-    for(auto &i: commands){
-        std::transform(i.begin(), i.end(), i.begin(), [] (unsigned char c){ return std::tolower(c); });
-    }
+    // COM: ToLower()
+    for(auto &i: commands) std::transform(i.begin(), i.end(), i.begin(), [] (unsigned char c){ return std::tolower(c); });
 
     std::cout << "\n######### Transformed: " << std::endl;
     for(auto i: commands) std::cout << i << std::endl;
 
-    // RegEx fishing ############
-
-
-
-    // std::regex soh("[\x01]");
-    // std::regex eot("[\x04]");
+    // PART: FILTER FOR SOH & EOT.
     std::regex soh_eot("[\x01]|[\x04]");
 
     std::cout << "\n######### RegExing SOH/EOT" << std::endl;
 
-    for(size_t i = 0; i < rawVec.size(); i++){
+    // Check if ready for valdation [match.ready()]
 
+    for(size_t i = 0; i < rawVec.size(); i++){
         std::smatch match;
         std::string temp{(char)rawVec[i]};
         std::regex_search(temp, match, soh_eot);
-
         std::cout << std::boolalpha;
-        // std::cout << "Checked for Results: " << match.ready() << std::endl;
         std::cout << "Matches found: " << !match.empty() << std::endl;
         std::cout << "Number of matches: " << match.size() << std::endl;
-
         std::cout << std::endl;
     }
 
+    // PART: FILTER FOR KNOWN COMMANDS
     std::cout << "\n######### RegExing yo" << std::endl;
 
     std::regex knownCommands("leave|(list)*servers$|keepalive|[\\s]msg$|statusre(q$|sp$)");
 
     for(size_t i = 0; i < commands.size(); i++){
-
         std::smatch match;
         std::regex_search(commands[i], match, knownCommands);
-
         std::cout << std::boolalpha;
-        // std::cout << "Checked for Results: " << match.ready() << std::endl;
         std::cout << "Matches found: " << !match.empty() << std::endl;
         std::cout << "Number of matches: " << match.size() << std::endl;
-
         if(!match.empty()) std::cout << "The match: " << match.str() << "\nFor word: " << commands[i] << std::endl;
         else std::cout << "No match: " << commands[i] << std::endl;
-
         std::cout << std::endl;
     }
     return 0;
 }
-
-// char asciitolower(char in){
-//     if (in <= 'Z' && in >= 'A')
-//         return in - ('Z' - 'z');
-//     return in;
-// }
-// std::transform(data.begin(), data.end(), data.begin(), asciitolower);
 
 // This function stuffs 0x01 and 0x04 on either end.
 // could be used to construct outgoing messages.
@@ -115,5 +103,3 @@ std::vector<std::byte> getRawBytes(std::string str){
     // STATUSRESP,FROM_GROUP,TO_GROUP,<server, msgs held>,...
     // STATUSRESP,V_GROUP_2,I_1,V_GROUP4,20,V_GROUP71,2
 // PART:
-
-// Aexx
